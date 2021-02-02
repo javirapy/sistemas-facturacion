ee51b6997f1e4bfefd89e3f63113c77df3a7406f
javira <vrjaracontacto@gmail.com>
2021-02-02 17:40:16 -0300

    Se agrego el router para el manejo de los enlaces

--- src/app/clientes/cliente.service.ts
+++ src/app/clientes/cliente.service.ts
@@ -1,6 +1,7 @@
 import { Injectable } from '@angular/core';
 import { CLIENTES } from './clientes.json';
 import { Cliente } from './cliente';
+import { Observable, of } from 'rxjs';
 
 
 @Injectable({
@@ -10,7 +11,7 @@ export class ClienteService {
 
   constructor() { }
 
-  getClientes(): Cliente[]{
-    return CLIENTES;
+  getClientes(): Observable<Cliente[]> {
+    return of(CLIENTES);
   }
 }
--- src/app/clientes/clientes.component.ts
+++ src/app/clientes/clientes.component.ts
@@ -12,7 +12,9 @@ export class ClientesComponent implements OnInit {
   constructor(private  clienteService: ClienteService) { }
 
   ngOnInit(): void {
-    this.clientes = this.clienteService.getClientes();
+    this.clienteService.getClientes().subscribe(
+      clientes =>  this.clientes = clientes
+    );
   }
 
 }
