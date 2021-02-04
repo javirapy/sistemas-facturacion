130dfa5d67cd9922562b3061977fa012d5cfede3
javira <vrjaracontacto@gmail.com>
2021-02-04 15:50:53 -0300

    Usando rest backend para listar clientes

--- src/app/clientes/cliente.service.ts
+++ src/app/clientes/cliente.service.ts
@@ -2,16 +2,24 @@ import { Injectable } from '@angular/core';
 import { CLIENTES } from './clientes.json';
 import { Cliente } from './cliente';
 import { Observable, of } from 'rxjs';
+import { HttpClient} from '@angular/common/http';
+import { map } from 'rxjs/operators'
+
 
 
 @Injectable({
   providedIn: 'root'
 })
-export class ClienteService {
 
-  constructor() { }
+export class ClienteService {
+  private urlEndPoint: string = 'http://localhost:8080/api/clientes';
+  constructor( private http : HttpClient ) { }
 
   getClientes(): Observable<Cliente[]> {
-    return of(CLIENTES);
+   // return of(CLIENTES);
+   //return this.http.get<Cliente[]>(this.urlEndPoint);
+   return this.http.get(this.urlEndPoint).pipe(
+     map( response => response as Cliente[])
+   );
   }
 }
